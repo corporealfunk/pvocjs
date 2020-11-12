93800344b301ef7bfc2542b6174c856902e3c6cb
Jon Moniaci <jonmoniaci@gmail.com>
2020-11-12 18:11:58 -0500

    Fix the aiff jest test

--- src/aiff.test.js
+++ src/aiff.test.js
@@ -5,11 +5,11 @@ describe('#open', () => {
     const aiff = new Aiff('./test/strings.aif');
 
     return aiff.open().then((data) => {
-      expect(aiff.formChunk).objectContaining({
+      expect(aiff.formChunk).toEqual(expect.objectContaining({
         ckId: 'FORM',
         size: 2288836,
         formType: 'AIFF',
-      });
+      }));
     });
   });
 });
